ebe86a31c2e33361660845b87051825a085e9b71
Thaumanovic <thaumanovic@forceofelites.com>
2020-11-20 14:43:58 +0000

    :p

--- Gw2Utils.Tests/Gw2TimeUtilsTests.cs
+++ Gw2Utils.Tests/Gw2TimeUtilsTests.cs
@@ -1,17 +1,55 @@
+using System;
 using NUnit.Framework;
+using Shouldly;
 
 namespace Gw2Utils.Tests
 {
-    public class Tests
+    public class Gw2TimeUtilsTests
     {
-        [SetUp]
-        public void Setup()
-        { }
+        /// <summary>
+        /// Valid times observed in-game.
+        /// </summary>
+        /// <remarks>
+        /// These can be obtained by entering "/time" into the game chat as soon as the in-game server time clock ticks
+        /// over to the next minute.
+        ///
+        /// If not done as such, there can be slight discrepancies as the in-game server time clock unfortunately
+        /// does not show the current seconds and relying on the current seconds of another time source may be inaccurate
+        /// due to slight differences in the current seconds.
+        /// </remarks>
+        internal static readonly object[] ValidTimeConversions = new[] {
+            new object[] { DateTime.Parse("08:50"), "10:00" },
+            new object[] { DateTime.Parse("08:53"), "10:36" }
+        };
 
-        [Test]
-        public void Test1()
+        internal static readonly object[] ValidTimesToTimeOfDayConversions = new[] {
+            new  object[] { TimeSpan.Parse("00:00"), TimeOfDay.Night },
+            new  object[] { TimeSpan.Parse("04:59"), TimeOfDay.Night },
+            new  object[] { TimeSpan.Parse("05:00"), TimeOfDay.Dawn },
+            new  object[] { TimeSpan.Parse("05:59"), TimeOfDay.Dawn },
+            new  object[] { TimeSpan.Parse("06:00"), TimeOfDay.Day },
+            new  object[] { TimeSpan.Parse("19:59"), TimeOfDay.Day },
+            new  object[] { TimeSpan.Parse("20:00"), TimeOfDay.Dusk },
+            new  object[] { TimeSpan.Parse("20:59"), TimeOfDay.Dusk },
+            new  object[] { TimeSpan.Parse("21:00"), TimeOfDay.Night },
+            new  object[] { TimeSpan.Parse("23:59"), TimeOfDay.Night }
+        };
+
+        [TestCaseSource(nameof(ValidTimeConversions))]
+        public void TestConvertsToTyrianTimeCorrectly(DateTime realTime, string expectedTyrianTime)
         {
-            Assert.Pass();
+            var currentTyrianCycleTime = Gw2TimeUtils.ConvertToTyrianTime(realTime);
+            
+            currentTyrianCycleTime.ToString("hh\\:mm")
+                .ShouldBe(expectedTyrianTime);
+        }
+
+        [TestCaseSource(nameof(ValidTimesToTimeOfDayConversions))]
+        public void TestConvertsFromTimeSpanToTimeOfDayCorrectly(TimeSpan time, TimeOfDay expectedTimeOfDay)
+        {
+            var actualTimeOfDay = Gw2TimeUtils.GetTyrianTimeOfDay(time);
+            
+            actualTimeOfDay.ShouldBe(expectedTimeOfDay);
         }
     }
 }
--- Gw2Utils/Gw2TimeUtils.cs
+++ Gw2Utils/Gw2TimeUtils.cs
@@ -1,7 +1,102 @@
-﻿namespace Gw2Utils
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gw2Utils
 {
-    public class Gw2TimeUtils
+    public enum TimeOfDay
     {
+        Dawn,
+        Day,
+        Dusk,
+        Night
+    }
+    
+    public static class Gw2TimeUtils
+    {
+        /// <summary>
+        /// The number of seconds in a day.
+        /// </summary>
+        private const int SecondsInADay = 86400;
+
+        /// <summary>
+        /// The number of game days that occur with a real-time day.
+        /// </summary>
+        private const int GameDaysInRealDay = 12;
         
+        /// <summary>
+        /// Used to convert the current time of day (in Tyria) expressed as a second to a game day stage.
+        /// </summary>
+        private static readonly Dictionary<Range, TimeOfDay> TyrianSecondsToDayStateConversions = new Dictionary<Range, TimeOfDay>() {
+            [0..17999] = TimeOfDay.Night,
+            [18000..21599] = TimeOfDay.Dawn,
+            [21600..71999] = TimeOfDay.Day,
+            [72000..75599] = TimeOfDay.Dusk,
+            [75600..86399] = TimeOfDay.Night
+        };
+        
+        /// <summary>
+        /// Converts a real-world time to the equivalent Tyrian time.
+        /// </summary>
+        public static TimeSpan ConvertToTyrianTime(DateTime realTime, int gameDaysInRealDay = GameDaysInRealDay)
+        {
+            TimeSpan currentDayTimespan;
+            double currentCycleSeconds;
+
+            // Retrieves a timespan that represents the time from 00:00 of the given realTime to the current time
+            // of the given realTime
+            currentDayTimespan = realTime - realTime.Date;
+
+            /**
+             * A single Tyrian day consists of 7200 real-time seconds, so we divide the current real-time seconds of
+             * the day by 7200 and then return the remainder - this gives us the current total seconds that have passed
+             * in Tyria for the current Tyrian day/cycle.
+             */
+            currentCycleSeconds = currentDayTimespan.TotalSeconds % (SecondsInADay / GameDaysInRealDay);
+
+            /**
+             * For every second that passes in real time, 12 pass in Tyrian time, so we simply need to multiply
+             * the current cycle seconds by 12 to convert them from a 2 hour cycle to a 24 hour cycle which will represent
+             * the time passed for the current day in Tyria.
+             */
+            return TimeSpan.FromSeconds(currentCycleSeconds * GameDaysInRealDay);
+        }
+
+        /// <summary>
+        /// Converts the current server time to Tyrian time.
+        /// </summary>
+        public static TimeSpan GetCurrentTyrianTime()
+        {
+            return ConvertToTyrianTime(DateTime.UtcNow);
+        }
+
+        /// <summary>
+        /// Returns the TimeOfDay for the specified Tyrian time.
+        /// </summary>
+        public static TimeOfDay GetTyrianTimeOfDay(TimeSpan tyrianTime)
+        {
+            if (tyrianTime.TotalSeconds >= SecondsInADay)
+                throw new ArgumentException("Must be a TimeSpan of less than 1 day.", nameof(tyrianTime));
+
+            foreach (var dayStateConversion in TyrianSecondsToDayStateConversions) {
+
+                var currentStageCheck = dayStateConversion.Key;
+                
+                if (tyrianTime.TotalSeconds >= currentStageCheck.Start.Value && tyrianTime.TotalSeconds <= currentStageCheck.End.Value) {
+                    return dayStateConversion.Value;
+                }
+            }
+
+            throw new Exception("Could not convert TimeSpam to TimeOfDay");
+        }
+
+        /// <summary>
+        /// Returns the TimeOfDay that represents the current time of day in Tyria.
+        /// </summary>
+        /// <returns></returns>
+        public static TimeOfDay GetCurrentTyrianTimeOfDay()
+        {
+            return GetTyrianTimeOfDay(GetCurrentTyrianTime());
+        }
     }
 }
